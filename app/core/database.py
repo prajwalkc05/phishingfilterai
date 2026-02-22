dce56eaa84b5071136dba3d0afbb281482a65692
Prajwal KC <prajwalkc05@gmail.com>
2026-02-22 20:04:31 +0530

    Update database configuration

--- app/core/database.py
+++ app/core/database.py
@@ -1,8 +1,6 @@
 from pymongo import MongoClient
 import os
 
-client = MongoClient(os.getenv("MONGO_URI", "mongodb://localhost:27017"))
-
+client = MongoClient(os.getenv("MONGO_URI"))
 db = client["phishing_ai"]
-
 feedback_collection = db["feedback"]
