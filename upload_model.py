6d36fd1029756224103d12afddcb32025e5947fb
Prajwal KC <prajwalkc05@gmail.com>
2026-02-26 21:25:12 +0530

    Clean up unnecessary files

--- upload_model.py
+++
@@ -1,9 +0,0 @@
-from huggingface_hub import upload_folder
-
-upload_folder(
-    folder_path="final_model",
-    repo_id="prajwalkc/phishing-bert",
-    repo_type="model"
-)
-
-print("Model uploaded successfully")
